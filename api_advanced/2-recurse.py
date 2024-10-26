fdb3606c0c06915b1f59846cc4c53e0d2cf1d5fb
SibahleD <s.dlamini2@alustudent.com>
2024-10-26 10:37:03 +0000

    jj

---
+++ api_advanced/2-recurse.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+recuse
+"""
+
+import requests
+
+def recurse(subreddit, hot_list=[], after=None):
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    params = {"limit": "100", "after": after}
+    response = requests.get(url, params=params, allow_redirects=False)
+    if response.status_code != 200:
+        return None
+    posts = response.json().get("data", {}).get("children", [])
+    for post in posts:
+        hot_list.append(post["data"]["title"])
+
+    after = response.json().get("data", {}).get("after", None)
+
+    if after is not None:
+        return recurse(subreddit, hot_list, after)
+    else:
+        return hot_list
