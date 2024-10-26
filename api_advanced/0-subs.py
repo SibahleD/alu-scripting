6202f6152ed3c4e18a566d03d357d9e7fd7b8fa1
SibahleD <s.dlamini2@alustudent.com>
2024-10-26 09:37:25 +0200

    jj

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -5,7 +5,14 @@ import requests
 
 """Module"""
 
-def number_of_subscribers(subreddit):
-    response = requests.get("https://www.reddit.com/r/{}.json".format(subreddit))
+
+def number_of_subscribers(sub):
+    headers = {"User-Agent": "subreddit-subscriber-counter"}
+    response = requests.get("https://www.reddit.com/r/{}.json".format(sub), headers=headers)
+    if response.status_code != 200:
+        return 0
     json_response = response.json
-    print(json_response['data']['subscribers'])
+    try:
+        return json_response['data']['children'][0]['data']['subreddit_subscribers']
+    except (KeyError, IndexError):
+        return 0
