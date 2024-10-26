eaa0becd6e1d0bb36360ffd15b6777aa0af9c500
SibahleD <s.dlamini2@alustudent.com>
2024-10-26 09:01:41 +0200

    num1

---
+++ api-advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Finding Number of subscribers in a given subbreddit
+"""
+import requests
+
+headers = {
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
+    'sec-ch-ua': '"Chromium";v="124", "Google Chrome";v="124", "Not-A.Brand";v="99"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+}
+
+
+def number_of_subscribers(subreddit):
+    response = requests.get(f'https://www.reddit.com/r/{subreddit}.json', headers=headers)
+    json_response = response.json
+    print(json_response['data']['subscribers'])
