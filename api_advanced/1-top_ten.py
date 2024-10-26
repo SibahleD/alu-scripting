fefdcae6b33c25fd3c2aee0f345c69bb6ebc0f23
SibahleD <s.dlamini2@alustudent.com>
2024-10-26 09:44:37 +0200

    gg

---
+++ api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Module for fetching top 10 hot post titles from a subreddit."""
+
+import requests
+
+
+def top_ten(subreddit):
+        headers = {"User-Agent": "subreddit-hot-posts-fetcher"}
+        response = requests.get(
+        "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit),
+        headers=headers,
+        allow_redirects=False
+    )
+        if response.status_code != 200:
+            print(None)
+            return
+        json_response = response.json()
+        try:
+            posts = json_response['data']['children']
+            for post in posts:
+                print(post['data']['title'])
+        except (KeyError, IndexError):
+            print(None)
