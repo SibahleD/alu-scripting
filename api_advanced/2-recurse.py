c2ff8e63ebc38a1a0102c394b9198915d611d4f2
SibahleD <s.dlamini2@alustudent.com>
2024-10-26 10:42:34 +0000

    higgin

--- api_advanced/2-recurse.py
+++ api_advanced/2-recurse.py
@@ -6,6 +6,7 @@ recuse
 
 import requests
 
+
 def recurse(subreddit, hot_list=[], after=None):
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     params = {"limit": "100", "after": after}
